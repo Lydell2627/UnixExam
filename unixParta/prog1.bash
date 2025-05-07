e6a1b0e597cf8aa655515a41d36512c4fcfc5f20
Lydell Dsouza <113416533+Lydell2627@users.noreply.github.com>
2025-05-07 06:34:57 +0530

    Update prog1.bash

--- unixParta/prog1.bash
+++ unixParta/prog1.bash
@@ -2,16 +2,16 @@ echo "enter the 3 numbers"
 read a
 read b
 read c
-if [ $a -ge $b ]&&[ $a -ge $b ];then
+if [ $a -ge $b ]&&[ $a -ge $c ];then
 	echo "$a is the greatest"
-elif [ $c -ge $b ]&&[ $c -ge $b ];then
+elif [ $c -ge $b ]&&[ $c -ge $a ];then
 	 echo "$c is the greatest"
 else
       	echo "$b is the greatest"
 fi
-if [ $a -le $b ]&&[ $a -le $b ];then
+if [ $a -le $b ]&&[ $a -le $c ];then
         echo "$a is the leastest"
-elif [ $c -le $b ]&&[ $c -le $b ];then
+elif [ $c -le $b ]&&[ $c -le $a ];then
          echo "$c is the least"
 else
         echo "$b is the least"
